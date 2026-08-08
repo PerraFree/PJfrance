13d4e1599e0a8c5740733061f4d7d4d66b7db9cc
PerraFree <PerraFree@users.noreply.github.com>
2026-08-08 15:00:23 +0000

    deploy: 5c42ecb091f5e93e4b17643d8292e81e34595d29

--- assets/index-CcxxOsPN.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DtoFRNX3.js","assets/index-CEZs7RNG.js","assets/leaflet-BPiNfx-7.js","assets/index-CeRk8SsT.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-CEZs7RNG.js";import"./leaflet-BPiNfx-7.js";const i=r("SplashScreen",{web:()=>t(()=>import("./web-DtoFRNX3.js"),__vite__mapDeps([0,1,2,3])).then(e=>new e.SplashScreenWeb)});export{i as SplashScreen};
---
+++ assets/web-CK_yG92f.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-B2XdsSX_.js";import"./leaflet-BPiNfx-7.js";class i extends n{async show(e){}async hide(e){}}export{i as SplashScreenWeb};
