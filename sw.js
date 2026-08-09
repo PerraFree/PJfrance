f647e12266bad48cf1e568948e86a30143b53202
PerraFree <PerraFree@users.noreply.github.com>
2026-08-09 17:46:15 +0000

    deploy: f5d566a556e1d380d18e5d15b0da9e36936b525c

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Enkel service worker: cachar appskal + stationsdata så appen fungerar offline. */
-const CACHE = 'tomningskartan-v7'
+const CACHE = 'tomningskartan-v8'
 const APP_SHELL = [
   './',
   './manifest.webmanifest',
