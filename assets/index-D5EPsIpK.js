8c65605b6e5caa83c841186d1baae11054496da7
PerraFree <PerraFree@users.noreply.github.com>
2026-09-06 21:09:05 +0000

    deploy: 9dc66fd6a8d3d275488880b807723f1d2b05c017

---
+++ assets/index-D5EPsIpK.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DaFLroTm.js","assets/index-DmeenPqK.js","assets/leaflet-BPiNfx-7.js","assets/index-vvoB3b3j.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-DmeenPqK.js";import"./leaflet-BPiNfx-7.js";const i=r("SplashScreen",{web:()=>t(()=>import("./web-DaFLroTm.js"),__vite__mapDeps([0,1,2,3])).then(e=>new e.SplashScreenWeb)});export{i as SplashScreen};
