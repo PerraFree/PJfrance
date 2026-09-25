caf5fd87cb69e28747442d7ebb82c839180eb997
PerraFree <PerraFree@users.noreply.github.com>
2026-09-25 10:45:56 +0000

    deploy: 3a3c2272e79930bda8c0485e23f61a69c4ece13a

---
+++ assets/index-CIOkjIG4.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-COVHK1ht.js","assets/index-CdwQU8BA.js","assets/leaflet-BPiNfx-7.js","assets/index-CPCaM4jX.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CdwQU8BA.js";import"./leaflet-BPiNfx-7.js";const i=r("Share",{web:()=>t(()=>import("./web-COVHK1ht.js"),__vite__mapDeps([0,1,2,3])).then(e=>new e.ShareWeb)});export{i as Share};
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Enkel service worker: cachar appskal + stationsdata så appen fungerar offline. */
-const CACHE = 'tomningskartan-v62'
+const CACHE = 'tomningskartan-v63'
 const APP_SHELL = [
   './',
   './manifest.webmanifest',
